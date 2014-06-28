a73a4ebb1724ab5046f707d2070fa4210fc99a6a
Benjamin Rogge <mail@benjamin-rogge.de>
2014-06-28 22:39:57 +0200

    back button from adding a mapping goes back to mapping activity now

--- app/src/main/java/de/plastickarma/readlater/app/CategoryMappingActivity.java
+++ app/src/main/java/de/plastickarma/readlater/app/CategoryMappingActivity.java
@@ -15,6 +15,8 @@ import android.view.MenuItem;
 public final class CategoryMappingActivity extends ActionBarActivity implements CategoryMappingHandler {
 
     public static final String CATEGORY_MAPPING_PREFS = "categoryMappings";
+    private Menu menu;
+    private boolean showingMappingDetails = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,55 +29,83 @@ public final class CategoryMappingActivity extends ActionBarActivity implements 
         final FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.container, mappingFragment);
         fragmentTransaction.commit();
-
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(final Menu menu) {
         getMenuInflater().inflate(R.menu.category_mapping, menu);
+        this.menu = menu;
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(final MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.action_settings) {
             Intent intent = new Intent(this, Settings.class);
             startActivity(intent);
             return true;
         } else if (id == R.id.action_add_mapping) {
-            final FragmentManager fragmentManager = getSupportFragmentManager();
-            final FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-            final CategoryMappingDetailFragment fragment = new CategoryMappingDetailFragment(this, null);
-            fragmentTransaction.replace(R.id.container, fragment);
-            fragmentTransaction.commit();
+           showCategoryMappingDetails(null);
         }
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public void handleEditMapping(final CategoryMapping mapping) {
+    /**
+     * Creates a new {@link CategoryMappingDetailFragment} and displays with the given category mapping. The mapping
+     * can be <code>null</code>.
+     */
+    private void showCategoryMappingDetails(final CategoryMapping mapping) {
+        final MenuItem item = menu.findItem(R.id.action_add_mapping);
+        item.setVisible(false);
         final FragmentManager fragmentManager = getSupportFragmentManager();
         final FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         final CategoryMappingDetailFragment fragment = new CategoryMappingDetailFragment(this, mapping);
         fragment.setOnSaveHook(new Runnable() {
             @Override
             public void run() {
-                CategoryMapping.removeCategoryMapping(CategoryMappingActivity.this, mapping);
+                item.setVisible(true);
+                setShowingMappingDetails(false);
+
+                if (mapping != null) {
+                    CategoryMapping.removeCategoryMapping(CategoryMappingActivity.this, mapping);
+                }
             }
         });
         fragmentTransaction.replace(R.id.container, fragment);
+        this.showingMappingDetails = true;
         fragmentTransaction.commit();
+    }
 
+    private void showAllCategories() {
+        this.showingMappingDetails = false;
+        final FragmentManager fragmentManager = this.getSupportFragmentManager();
+        final FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.container, new CategoryMappingsFragment(this));
+        fragmentTransaction.commit();
+    }
+
+    private void setShowingMappingDetails(final boolean value) {
+        this.showingMappingDetails = value;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(this.showingMappingDetails) {
+            showAllCategories();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public void handleEditMapping(final CategoryMapping mapping) {
+        showCategoryMappingDetails(mapping);
     }
 
     @Override
     public void handleCreateMapping(final CategoryMapping mapping) {
         CategoryMapping.addCategoryMapping(this, mapping);
-
-        final FragmentManager fragmentManager = this.getSupportFragmentManager();
-        final FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.container, new CategoryMappingsFragment(this));
-        fragmentTransaction.commit();
+        showAllCategories();
     }
 }
